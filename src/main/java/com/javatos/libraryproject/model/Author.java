aa5383396e56cc764af1a9d3dc79f62cdf64d8a0
Airton <airton.manfrini@hotmail.com>
2023-01-13 22:01:52 -0300

    fix: author is now a person

--- src/main/java/com/javatos/libraryproject/model/Author.java
+++ src/main/java/com/javatos/libraryproject/model/Author.java
@@ -3,8 +3,12 @@ package com.javatos.libraryproject.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
 @Entity
 @Data
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Author extends Person{
     private String description;
 
--- src/main/java/com/javatos/libraryproject/model/Person.java
+++ src/main/java/com/javatos/libraryproject/model/Person.java
@@ -1,13 +1,11 @@
 package com.javatos.libraryproject.model;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Email;
 import java.util.Date;
 
 @Entity
