db3ebea7031fa68f59c5a53ab7b2002a85e6fba0
Airton <airton.manfrini@hotmail.com>
2023-01-18 21:31:40 -0300

    add: Cheking java constraints

--- src/main/java/com/javatos/libraryproject/model/Author.java
+++ src/main/java/com/javatos/libraryproject/model/Author.java
@@ -8,7 +8,7 @@ import javax.persistence.InheritanceType;
 
 @Entity
 @Data
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Author extends Person{
     private String description;
 }
