d796fbb4fe3a019383185ebaac47ec312202689a
WillikinsM <willikins98@gmail.com>
2023-01-27 17:55:02 -0300

    delete old version

--- library-registry/src/main/java/com/atos/library/libraryregistry/model/Role.java
+++
@@ -1,9 +0,0 @@
-package com.atos.library.libraryregistry.model;
-
-public enum Role {
-
-    ADMIN,
-    EMPLOYEE,
-    CUSTOMER
-
-}
--- library-registry/src/main/java/com/atos/library/libraryregistry/repository/PublisherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.atos.library.libraryregistry.repository;
-
-import com.atos.library.libraryregistry.model.Publisher;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PublisherRepository extends MongoRepository<Publisher, String> {
-}
