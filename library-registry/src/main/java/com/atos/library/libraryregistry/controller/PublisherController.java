282824bd450877c06494def72e8f3a62b9bdc27c
Juliano <juliano.vieira@atos.net>
2023-01-28 11:58:53 -0300

    V2

---
+++ library-registry/src/main/java/com/atos/library/libraryregistry/controller/PublisherController.java
@@ -0,0 +1,53 @@
+package com.atos.library.libraryregistry.controller;
+
+import com.atos.library.libraryregistry.model.Publisher;
+import com.atos.library.libraryregistry.service.PublisherService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/publisher")
+@Slf4j
+public class PublisherController {
+
+    private final PublisherService publisherService;
+
+    @GetMapping("/{id}")
+    public Publisher findById(@PathVariable String id) {
+        return publisherService.findById(id);
+    }
+
+    @GetMapping
+    public List<Publisher> findAll() {
+        return publisherService.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Publisher create(@Valid @RequestBody Publisher publisher) {
+        log.info("iniciando o cadastro de um editor: {}", publisher);
+        return publisherService.create(publisher);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable String id) {
+        log.warn("excluindo um editor");
+        publisherService.delete(id);
+        log.warn("editor excluido");
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseBody
+    public void Update(@PathVariable Publisher publisher) {
+        publisherService.update(publisher);
+
+    }
+}
