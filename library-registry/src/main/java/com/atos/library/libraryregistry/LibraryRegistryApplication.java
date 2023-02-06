38000188645461b74f59419993e0d5d915959c16
Juliano Vieira <juliano.vieira@atos.net>
2023-02-05 21:39:41 -0300

    V4

--- library-registry/src/main/java/com/atos/library/libraryregistry/LibraryRegistryApplication.java
+++ library-registry/src/main/java/com/atos/library/libraryregistry/LibraryRegistryApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -20,19 +22,24 @@ public class LibraryRegistryApplication {
 	}
 
 	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
 	CommandLineRunner run(UserService userService) {
 		return args -> {
 			System.out.println("Rodandooooooooo...............");
 
-		userService.saveRole(new Role(null, "ADMIN"));
-        userService.saveRole(new Role(null, "EMPLOYEE"));
-        userService.saveRole(new Role(null, "CUSTOMER"));
+//		userService.saveRole(new Role(null, "ADMIN"));
+//        userService.saveRole(new Role(null, "EMPLOYEE"));
+//        userService.saveRole(new Role(null, "CUSTOMER"));
 
-        userService.saveUser(new User(null, "MainApp", "abcdddd@atos.net", "010203", new ArrayList<>()));
-        userService.saveUser(new User(null, "Juliano Vieira", "proprietyofkurd@hotmail.com", "123456", new ArrayList<>()));
+//        userService.saveUser(new User(null, "Juliano Teste", "teste@teste.com", "123456", new ArrayList<>()));
+//        userService.saveUser(new User(null, "Juliano Vieira", "proprietyofkurd@hotmail.com", "123456", new ArrayList<>()));
 //
 
-        userService.addRoleToUser("abcdddd@atos.net", "ADMIN");
+//        userService.addRoleToUser("proprietyofkurd@hotmail.com", "ADMIN");
 //		userService.addRoleToUser("abcdddd@atos.net", "ADMIN2");
 //			userService.addRoleToUser("juliano.vieira@atos.net", "ADMIN");
 
